49525c06dab4ffddb1e397d27ba93dd447381de9
Ben Schwartz <benschw@gmail.com>
2015-02-19 14:19:23 -0600

    test(dns): testing srv record lookup

--- test/DnsResolverTest.php
+++ test/DnsResolverTest.php
@@ -3,13 +3,45 @@
 namespace Benschw\Consul;
 
 
+
 class DnsResolverTest extends \PHPUnit_Framework_TestCase {
 
 
-	public function testName() {
-		$resv = new DnsResolver("google.com", DNS_A);
-		$addr = $resv->resolve();
-		$this->assertSame('127.0.0.1', $addr);
+	public function testSRVLookup() {
+		// given
+		$expected = array(array(
+			'host' => 'foo.service.fliglio.com',
+			'class' => 'IN',
+			'ttl' => 14382,
+			'type' => 'SRV',
+			'pri' => 1,
+			'weight' => 1,
+			'port' => 8001,
+			'target' => 'foo1.fliglio.com'
+		), array(
+			'host' => 'foo.service.fliglio.com',
+			'class' => 'IN',
+			'ttl' => 14382,
+			'type' => 'SRV',
+			'pri' => 1,
+			'weight' => 1,
+			'port' => 8002,
+			'target' => 'foo2.fliglio.com'
+		));
+
+		// when
+		$resv = new DnsResolver("foo.service.fliglio.com", DNS_SRV);
+		$found = $resv->resolve();
+
+		// sort by target since order isn't deterministic
+		usort($found, function($a, $b) {
+			return strcmp($a['target'], $b['target']);
+		});
+
+		// then
+		$this->assertEquals($expected[0]['target'], $found[0]['target']);
+		$this->assertEquals($expected[1]['target'], $found[1]['target']);
+		$this->assertEquals(count($expected), count($found));
 
 	}
 
