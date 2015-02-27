40662e0d6d4950fe619be5d5ba0b7c0004b1b7eb
Ben Schwartz <benschw@gmail.com>
2015-02-27 15:09:32 -0600

    refactor(uri): updating to use Url object

--- Fliglio/Consul/DnsResolver.php
+++ Fliglio/Consul/DnsResolver.php
@@ -1,7 +1,7 @@
 <?php
 namespace Fliglio\Consul;
 
-use Fliglio\Web\Uri;
+use Fliglio\Web\Url;
 
 class DnsResolver {
 	
@@ -12,7 +12,7 @@ class DnsResolver {
  		$record = dns_get_record($name, $type);
  		$mapped = array();
  		foreach ($record as $address) {
- 			$mapped[] = Uri::fromHostAndPort($address['target'], $address['port']);
+ 			$mapped[] = Url::fromHostAndPort($address['target'], $address['port']);
  		}
  		return $mapped;
 	}
