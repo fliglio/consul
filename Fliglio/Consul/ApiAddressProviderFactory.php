752f4938dc1c41949a8f413931a5bb7b49fd927e
Ben Schwartz <benschw@gmail.com>
2017-06-06 12:29:49 -0500

    adding api support for service discovery

---
+++ Fliglio/Consul/ApiAddressProviderFactory.php
@@ -0,0 +1,30 @@
+<?php
+namespace Fliglio\Consul;
+
+class ApiAddressProviderFactory {
+
+	private $resolver;
+
+	public function __construct(Resolver $resolver = null) {
+		if (is_null($resolver)) {
+			$baseUri = 'http://127.0.0.1:8500';
+			if (getenv('CONSUL_HTTP_ADDR') !== false) {
+				$baseUri = getenv('CONSUL_HTTP_ADDR');
+			}
+			$options = [
+				'http_errors' => false,
+			];
+			$http = new \GuzzleHttp\Client($options);
+			$resolver = new ApiResolver($http, [
+				'ConsulAddr' => $baseUri,
+			]);
+		}
+		$this->resolver = $resolver;
+	}
+
+	public function create($name) {
+		$lb = new ConsulLoadBalancer($this->resolver, new RoundRobinLoadBalancerStrategy(), $name);
+		return new ConsulAddressProvider($lb);
+	}
+}
+
---
+++ Fliglio/Consul/ApiResolver.php
@@ -0,0 +1,52 @@
+<?php
+namespace Fliglio\Consul;
+
+use Fliglio\Web\Url;
+
+class ApiResolver implements Resolver {
+
+	private $http;
+	private $opts;
+
+	public function __construct(\GuzzleHttp\Client $http, $opts=[]) {
+		$this->http = $http;
+		$this->opts = $opts;
+	}
+	
+	public function resolve($name) {
+		$uri = $this->opts['ConsulAddr'] . '/v1/catalog/service/' . $name;
+
+		$request = $this->http->createRequest("GET", $uri);
+		$response = $this->http->send($request);
+		$status = $response->getStatusCode();
+
+		if ($status != 200) {
+			throw new \Exception($status . ": " . $response->getBody());
+		}
+
+		$data = json_decode($response->getBody(), true);
+
+		$mapped = array();
+		foreach ($data as $address) {
+			$url = Url::fromHostAndPort($address['ServiceAddress'], $address['ServicePort']);
+			
+			// if "Tags" option is set, only consider instances with matching tags
+			if (isset($this->opts['Tags'])) {
+				$fail = false;
+				foreach ($this->opts['Tags'] as $tag) {
+					if (!in_array($tag, $address['ServiceTags'])) {
+						$fail = true;
+					}
+				}
+				if (!$fail) {
+					$mapped[] = $url;
+				}
+			} else {
+				$mapped[] = $url;
+			}
+		}
+		return $mapped;
+	}
+
+}
+
