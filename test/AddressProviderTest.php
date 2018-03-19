5b0990f614e2c9bef23b86458458f285a1fc9c53
david byrd <david.byrd@bancvue.com>
2018-03-19 12:52:56 -0500

    ADDING_POSTFIXINESS Adding a static address generating provider factory

--- test/AddressProviderTest.php
+++ test/AddressProviderTest.php
@@ -3,6 +3,7 @@
 namespace Fliglio\Consul;
 
 use Fliglio\Web\Url;
+use GuzzleHttp\Client;
 
 class AddressProviderTest extends \PHPUnit_Framework_TestCase {
 
@@ -42,5 +43,57 @@ class AddressProviderTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals($expected->getHost(), $found->getHost());
 		$this->assertEquals($expected->getPort(), $found->getPort());
 	}
+
+    public function testAlbAddressProviderFactory() {
+        // given
+        $alb = new HostPrefixAddressProviderFactory('us');
+
+        // when
+        $provider = $alb->create('httpstat');
+
+        // then
+        $addr = $provider->getAddress();
+        $this->assertEquals([
+            'host' => 'httpstat.us',
+            'port' => 443
+        ], [
+            'host' => $addr->getHost(),
+            'port' => $addr->getPort()
+        ]);
+
+        // and
+        $http = new Client([
+            'base_url' => $addr->__tostring(),
+            'allow_redirects' => false
+        ]);
+        $resp = $http->get('/200');
+        $this->assertEquals('200 OK', $resp->getBody()->getContents());
+        $this->assertEquals('https://httpstat.us/200', $resp->getEffectiveUrl());
+    }
+
+    public function testAlbAddressProviderFactory_shouldSetSchemeHttp() {
+        // given
+        $alb = new HostPrefixAddressProviderFactory('google.com', 80);
+
+        // when
+        $provider = $alb->create('www');
+
+        // then
+        $addr = $provider->getAddress();
+        $this->assertEquals([
+            'host' => 'www.google.com',
+            'port' => 80
+        ], [
+            'host' => $addr->getHost(),
+            'port' => $addr->getPort()
+        ]);
+
+        // and
+        $http = new Client([
+            'base_url' => $addr->__tostring()
+        ]);
+        $resp = $http->get('/');
+        $this->assertEquals('http://www.google.com/', $resp->getEffectiveUrl());
+    }
 
 }
